88543f2781371da857af26f9d5d7e705150deb5f
MrKoijk <targiel.t@gmail.com>
2015-07-12 21:16:55 +0200

    Update executive.js

--- js/executive.js
+++ js/executive.js
@@ -1,1 +1,74 @@
+var forbiddenList_url = "https://raw.githubusercontent.com/MrKoijk/youtube-botkiller/master/json/forbidden.json";
+var forbidden_words;
 
+$(function() {
+	$.getJSON(forbidden-list_url, function(result) {
+		forbidden_words = result.words;
+	});
+	
+	// Wsparcie dla nowego YT (ładowanie strony przez AJAX bez przeładowywanie jej)
+	var interval = setInterval(function() {
+		scan();
+		
+		//clearInterval(interval);
+	}, 4000);
+	
+	scan();
+});
+
+var checkComment = function(comment) {
+	if(comment == null)
+		return false;
+	
+	for (i = 0; i < forbidden_words.length; i++) {
+		if(comment.includes(forbidden_words[i])) {
+			console.log("Niedozwolony string " + forbidden_words[i] + " w: " + comment);
+			return true;
+		}
+	}
+	return false;
+}
+
+var scan = function() {
+	
+	var target = document.querySelector('#watch-discussion');
+		
+	// Statystyki
+	if(document.getElementById("FakeCommentsHiderStats") == null) {
+		$("<div style=\"background-color: rgba(#696969, 0.64);\"><center>Usunięto <span id=\"FakeCommentsHiderStats\">0</span> nieprawdziwych komentarzy.</br><span style=\"font-size: 90%;\">W miare poruszania się po komentarzach i czytaniu odpowiedzi liczba będzie się zwiększać.</span></br></br><span style=\"font-size: 85%;\">Podoba Ci się ta wtyczka? Możesz wesprzeć mnie poprzez <a href=\"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=9PL5J7ULZQYJQ\" target=\"_blank\">PayPala</a> lub <a href=\"https://steamcommunity.com/tradeoffer/new/?partner=126623086&token=V3eGov0E\" target=\"_blank\">wysyłając mi jakieś skiny</a>. Za wszystkie dotacje bardzo dziękuje! :)</span></center></div></br>").insertBefore("#watch-discussion");
+	}
+	
+	if(target !== null) {
+		
+		// Set MutationObserver
+		var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+		var observer = new MutationObserver(function() {
+			execute();
+		});
+		 
+		var config = { childList: true, subtree: true };
+		 
+		observer.observe(target, config);
+		
+		// Execute removal a couple of times before MutationObserver kicks in
+		var counter = 0;
+		var interval = setInterval(function() {
+			execute();
+			
+			counter++;
+			if(counter === 24) {
+				clearInterval(interval);
+			}
+		}, 250);
+	}
+}
+
+Array.prototype.contains = function(obj) {
+    var i = this.length;
+    while (i--) {
+        if (this[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
